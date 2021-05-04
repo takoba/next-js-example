69bb52be974343f9b6f756e85eb85a03cd46a7e5
takoba <plane25@gmail.com>
2021-05-04 23:25:52 +0900

    :heavy_plus_sign: styled-componentsを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import 'ress'
 import { AppProps } from "next/app";
+import {createGlobalStyle, ThemeProvider} from "styled-components":w
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans JP', -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+`
+
+const Theme = {}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <GlobalStyle />
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
