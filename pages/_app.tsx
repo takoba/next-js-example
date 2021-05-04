8c29f4d8e22d45f3d722dbd4facb135be041f0be
takoba <plane25@gmail.com>
2021-05-04 23:17:17 +0900

    :art: add ress package

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import 'ress'
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
